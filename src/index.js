89e98c9aaa12921a7213bb373df47343e658ddf6
Emily Córdoba <vepicpz@gmail.com>
2021-04-27 09:19:19 -0500

    añadida clase triangulo e imagen

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-/* FUNCIONES SIMPLIFICADAS
-   Coordenadas Cartesianas
-   ------------------------------------------
-   Programa que contiene funciones similares a CANVAS
-   pero simplificadas y adaptadas al plano cartesiano
-
-   Autor: José Gilberto Vargas Cano (Docente)
-   Fecha: Abril 2021
-   Versión: 1.0
-*/
-
 const linea = new Linea(255, 200, 'red', 1);
 
 linea.mover(0, 0);
@@ -25,11 +14,32 @@ const rectangulo = new Rectangulo(60, 250, 120, 100);
 rectangulo.mover(40, 50);
 rectangulo.dibujar();
 
+const rectangulo2 = new Rectangulo(100, 300, 120, 100);
+
+rectangulo2.mover(40, 50);
+rectangulo2.dibujar();
+rectangulo2.relleno('green');
+
 const circulo = new Circulo(100, 300, 50, 'blue', 'black');
 circulo.dibujar();
 
 const arco = new Arco(400, 100, 50, 5, 2, 'blue', 'black');
 arco.dibujar();
 
-const texto = new Texto('hola', 100, 50, 'bold 50px monospace');
+const texto = new Texto('hola', 100, 50, 'bold 50px monospace', 'green');
 texto.dibujar();
+
+const triangulo = new Triangulo([100, 320, 350], [320, 350, 200], 'blue');
+triangulo.dibujar();
+
+let image = new Image();
+image.src = 'img/números.jpg';
+
+let imagen = new Imagen(image, 100, 100, 120);
+imagen.cargarImagen();
+
+var im2 = new Image();
+im2.src = 'img/números.jpg';
+
+let imagen2 = new Imagen(im2, 300, 100, 200, 300);
+imagen2.cargarImagen();
