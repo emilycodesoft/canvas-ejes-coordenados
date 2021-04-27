7487a8c717c375678df4d492b5358236aaf2be98
Emily Córdoba <vepicpz@gmail.com>
2021-04-27 17:49:27 -0500

    cambiadas nombres de algunas variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const cartesianoConEjesCoordenados = new EjesCoordenados(100, 100, 50, 50, 450, 250, 22, 'días', 'ventas', 1, 5);
 
+//resize cuando se pidan valores del cartesiano mas grandes
+// canvas ocupe todo el ancho
 cartesianoConEjesCoordenados.inicializar();
