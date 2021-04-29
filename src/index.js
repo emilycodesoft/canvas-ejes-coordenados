9912fcb23c222f64d37b157adbb83c2fc69e0a8f
Emily Córdoba <vepicpz@gmail.com>
2021-04-29 11:29:18 -0500

    Funcion cuadratica graficada

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-const cartesianoConEjesCoordenados = new EjesCoordenados(100, 100, 50, 50, 800, 400, 22, 'días', 'ventas', 1, 5);
+const cartesianoConEjesCoordenados = new EjesCoordenados(100, 100, 50, 50, 320, 420, 22, 'días', 'ventas', 1, 5);
 
 //enlazar datos con inputs
 //resize cuando se pidan valores del cartesiano mas grandes
 // canvas ocupe todo el ancho
 cartesianoConEjesCoordenados.inicializar();
-const triangulo = new Triangulo([200, 320, 300], [280, 220, 150], 'pink', 10);
-triangulo.dibujar();
